7147b5025d2e1681ecc87cc4d4ce1a4f8efbeca6
NiklasOPF <nikfor@student.chalmers.se>
2020-04-07 04:53:44 +0800

    working on the leave request for doctors

--- CARS/CARS-ejb/src/java/ejb/session/stateless/DoctorSessionBean.java
+++ CARS/CARS-ejb/src/java/ejb/session/stateless/DoctorSessionBean.java
@@ -5,16 +5,23 @@
  */
 package ejb.session.stateless;
 
+import com.sun.xml.rpc.processor.schema.UnimplementedFeatureException;
 import entity.DoctorEntity;
 import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Date;
+import java.time.DayOfWeek;
+import java.time.temporal.WeekFields;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import util.exception.DoubleLeaveRequestException;
+import util.exception.LeaveToCloseInTimeException;
 
 /**
  *
@@ -30,7 +37,48 @@ public class DoctorSessionBean implements DoctorSessionBeanRemote, DoctorSession
     
     @PersistenceContext(unitName = "CARS-ejbPU")
     private EntityManager em;
+    public void requestDoctorsLeave(Date date, Long doctorId) throws LeaveToCloseInTimeException, DoubleLeaveRequestException{//TODO throws ...
+        // Need to check for 1 week in advace, no appointments, no double leaves, only one day free per week.
+        // Note that checking for one booking per week also takes care of the duplicates
+        long dif1 = date.getTime() - new Date(Calendar.getInstance().getTime().getTime()).getTime();
+        Long dif2 = new Date(0, 0, 2).getTime() - new Date(0, 0, 0).getTime();
+        if (dif1<dif2){ // In case the time is to close 
+            throw new LeaveToCloseInTimeException();
+        }
+        // Check for duplicate bookings
+//        List ids = getDoctorsOnLeave(date);
+//        if(ids.contains(doctorId)){
+//            throw new DoubleLeaveRequestException();
+//        }
+        
+        // TODO check for appontment once that functionallity is set up
+        // Can only apply for one leave per week
+        Date monday = new Date(0);
+        Date friday = new Date(0);
+        Locale locale = null;
+        final DayOfWeek firstDayOfWeek = WeekFields.of(locale).getFirstDayOfWeek();
+        final DayOfWeek lastDayOfWeek = DayOfWeek.of(((firstDayOfWeek.getValue() + 5) % DayOfWeek.values().length) + 1);
+        System.err.println("asd");
+        
+        
+        
+    }
+    private List getDoctorsOnLeave(Date startDate, Date endDate){
+        Query query = em.createQuery("SELECT DISTINCT p.doctorEntity FROM DoctorsLeaveEntity p WHERE p.date = :date");
+        query.setParameter("date", startDate);
+        List doctorsIds =  query.getResultList();
+        
+        return doctorsIds;
+    }
     
+    public List getDoctorsOnLeave(Date date){
+        Query query2 = em.createQuery("SELECT DISTINCT p.doctorsLeaveId FROM DoctorsLeaveEntity p WHERE p.date = :date");
+        Query query = em.createQuery("SELECT DISTINCT p.doctorEntity FROM DoctorsLeaveEntity p WHERE p.date = :date");
+        query.setParameter("date", date);
+        List doctorsIds =  query.getResultList();
+        
+        return doctorsIds;
+    }
     
     @Override
     public List retrieveAllDoctors(){
@@ -86,6 +134,11 @@ public class DoctorSessionBean implements DoctorSessionBeanRemote, DoctorSession
     public void deleteDoctorEntity(Long doctorId){
         DoctorEntity doctorEntity = retrieveDoctorEntityByDoctorId(doctorId);
         em.remove(doctorEntity);
+    }
+
+    @Override
+    public Long[] getAvailableDoctors(java.util.Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
---
+++ CARS/CARS-ejb/src/java/ejb/session/stateless/tempMain.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.exception.DoubleLeaveRequestException;
+import util.exception.LeaveToCloseInTimeException;
+
+/**
+ *
+ * @author Niklas
+ */
+public class tempMain {
+    public static void main(String[] args){
+        DoctorSessionBean doc = new DoctorSessionBean();
+//        try {
+//            doc.requestDoctorsLeave(new Date(176685001 + Calendar.getInstance().getTime().getTime()), new Long(1));
+//        } catch (LeaveToCloseInTimeException ex) {
+//            Logger.getLogger(tempMain.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (DoubleLeaveRequestException ex) {
+//            Logger.getLogger(tempMain.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        doc.getDoctorsOnLeave(new Date(0, 0, 0));
+    }
+    
+}
