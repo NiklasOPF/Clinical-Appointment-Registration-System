4156300ca46a7e10d1b5eb88b70f4143b6acd7f0
NiklasOPF <nikfor@student.chalmers.se>
2020-04-14 17:14:09 +0000

    added (rough) initialization

--- CARS/CARS-ejb/src/java/ejb/session/singleton/DataInitializationSessionBean.java
+++ CARS/CARS-ejb/src/java/ejb/session/singleton/DataInitializationSessionBean.java
@@ -14,47 +14,26 @@ import javax.persistence.Query;
 @Startup
 @LocalBean
 @Singleton
-public class DataInitializationSessionBean { //TODO fix this bean so that the program does not crash
+public class DataInitializationSessionBean {
 
-    //@EJB(name = "StaffSessionBeanLocal")
-    //private StaffSessionBeanLocal staffSessionBeanLocal;
     @PersistenceContext(unitName = "CARS-ejbPU")
     private EntityManager em;
-
-    public DataInitializationSessionBean() {
-    }
+//TODO fix this bean so that the program does not crash
 
     @PostConstruct
-    public void postConstruct() {
-        try {
-            
-            // cant add a check for entity, either by calling an session bean or by using entiy manager
-            //if (staffSessionBeanLocal.retrieveStaffEntityByUserName("sdlsdf")==null){
-             //   throw new Exception();
-            
-            //em.persist(new StaffEntity("first", "last", "admin", "password")); // TODO this initializes an staffentity object, which in turn increments the ID. do this in a better way
-            //em.flush();
-           // Can directly try to input since we don't allow duplicate usernames
-           //em.persist(new StaffEntity("Linda", "Chua", "manager", "password")); // TODO this initializes an staffentity object, which in turn increments the ID. do this in a better way
-           //em.flush();
+    public void init() {
+        System.out.println("osödnfpoasözvnkjdfzvbpközdfbvözkxcsdofnsdkfnlkfnlsdknfl");
 
-            
-        } catch (Exception e) { // TODO make exception mor especific
-            em.persist(new StaffEntity("Linda", "Chua", "manager", "password")); // TODO this initializes an staffentity object, which in turn increments the ID. do this in a better way
+        try {
+            Query query = em.createQuery("SELECT DISTINCT p FROM StaffEntity p WHERE p.userName = :name");
+            query.setParameter("name", "bananer");
+            query.getResultList().get(0);
+        } catch (Exception e) {
+            StaffEntity staffEntity = new StaffEntity("apan", "torsten", "bananer", "pass");
+            em.persist(staffEntity);
             em.flush();
         }
-//        try
-//        {
-//            em.persist(new StaffEntity("first", "last", "admin", "password"));
-//            em.flush();
-//            StaffEntity admin = (StaffEntity) em.createQuery("SELECT DISTINCT p FROM StaffEntity p WHERE p.userName = admin").getResultList().get(0);
-//            //throw new Exception();
-//            //staffSessionBeanLocal.retrieveStaffEntityByUserName("admin");
-//
-//        }
-//        catch(Exception ex){
-//            em.persist(new StaffEntity("first", "last", "admin", "password"));
-//            em.flush();
+
     }
 
 }
